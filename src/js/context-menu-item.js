d74b0584aeebc4947013717d45e9419754706fcf
brandonocasey <brandonocasey@gmail.com>
2017-04-18 18:12:05 -0400

    jsdoc updates

--- src/js/context-menu-item.js
+++ src/js/context-menu-item.js
@@ -3,9 +3,25 @@ import videojs from 'video.js';
 
 const MenuItem = videojs.getComponent('MenuItem');
 
+/**
+ * A menu item for the context menu
+ *
+ * @extends MenuItem
+ */
 class ContextMenuItem extends MenuItem {
 
-  handleClick(e) {
+  /**
+   * Any click on a `MenuItem` puts int into the selected state.
+   * See {@link ClickableComponent#handleClick} for instances where this is called.
+   *
+   * @param {EventTarget~Event} event
+   *        The `keydown`, `tap`, or `click` event that caused this function to be
+   *        called.
+   *
+   * @listens tap
+   * @listens click
+   */
+  handleClick(event) {
     super.handleClick();
     this.options_.listener();
 
